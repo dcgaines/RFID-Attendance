0037c483dbf0585c7d1655eeeea7ac37377c9788
dcgaines <dcgaines@wowway.com>
2016-01-14 07:17:43 -0500

    Added test for print functions

--- RFIDTest.py
+++ RFIDTest.py
@@ -2,6 +2,11 @@
 
 import serial
 ser = serial.Serial('/dev/ttyAMA0', 2400, timeout=1)
+
+print "yes hello"
+print "this is a test of print"
+print "might need a main def"
+
 while True:
     string = ser.read(12)
     if len(string) != 0:
