488a3db1513eb1e0b210658cca20fb786f27f8f4
dcgaines <dcgaines@wowway.com>
2016-01-13 19:53:35 -0500

    Missed a slash

--- RFIDTest.py
+++ RFIDTest.py
@@ -1,7 +1,7 @@
 #Program to Test RFID reader
 
 import serial
-ser = serial.Serial('dev/ttyAMA0', 2400, timeout=1)
+ser = serial.Serial('/dev/ttyAMA0', 2400, timeout=1)
 while True:
     string = ser.read(12)
     if len(string) != 0:
