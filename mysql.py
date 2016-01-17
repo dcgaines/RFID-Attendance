395e92be57ef6aa1dd0e9561faf7e5485f4cd93c
dcgaines <dcgaines@wowway.com>
2016-01-17 16:58:50 -0500

    more spacing

--- mysql.py
+++ mysql.py
@@ -125,7 +125,7 @@ def viewAll():
     cur.execute("SELECT * FROM hours")
     rows = cur.fetchall()
     desc = cur.description
-    print "%s\t\t%s\t\t%s\t\t%s\t%s\t\t\t%s\t%s\t%s\n" % (desc[0][0],desc[1][0],desc[2][0],desc[3][0],desc[4][0],desc[5][0],desc[6][0],desc[7][0])
+    print "%s\t\t%s\t\t%s\t\t%s\t%s\t\t\t%s\t\t\t%s\t%s\n" % (desc[0][0],desc[1][0],desc[2][0],desc[3][0],desc[4][0],desc[5][0],desc[6][0],desc[7][0])
     for row in rows:
         if row[5] == None:
             if row[4] == None:
@@ -154,25 +154,25 @@ def viewAll():
             if row[4] == None:
                 if len(row[1]) <= 7:
                     if len(row[2]) <= 7:
-                        print "%s\t%s\t\t%s\t\t%s\t%s\t\t\t%s\t\t\t%s\t\t%s" % (row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7]) 
+                        print "%s\t%s\t\t%s\t\t%s\t%s\t\t\t%s\t%s\t\t%s" % (row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7]) 
                     else:
-                        print "%s\t%s\t\t%s\t%s\t%s\t\t\t%s\t\t\t%s\t\t%s" % (row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7])    
+                        print "%s\t%s\t\t%s\t%s\t%s\t\t\t%s\t%s\t\t%s" % (row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7])    
                 else:
                     if len(row[2]) <= 7:
-                        print "%s\t%s\t%s\t\t%s\t%s\t\t\t%s\t\t\t%s\t\t%s" % (row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7]) 
+                        print "%s\t%s\t%s\t\t%s\t%s\t\t\t%s\t%s\t\t%s" % (row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7]) 
                     else:
-                        print "%s\t%s\t%s\t%s\t%s\t\t\t%s\t\t\t%s\t\t%s" % (row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7])        
+                        print "%s\t%s\t%s\t%s\t%s\t\t\t%s\t%s\t\t%s" % (row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7])        
             else:
                  if len(row[1]) <= 7:
                     if len(row[2]) <= 7:
-                        print "%s\t%s\t\t%s\t\t%s\t%s\t%s\t\t\t%s\t\t%s" % (row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7]) 
+                        print "%s\t%s\t\t%s\t\t%s\t%s\t%s\t%s\t\t%s" % (row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7]) 
                     else:
-                        print "%s\t%s\t\t%s\t%s\t%s\t%s\t\t\t%s\t\t%s" % (row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7])    
+                        print "%s\t%s\t\t%s\t%s\t%s\t%s\t%s\t\t%s" % (row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7])    
                  else:
                     if len(row[2]) <= 7:
-                        print "%s\t%s\t%s\t\t%s\t%s\t%s\t\t\t%s\t\t%s" % (row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7]) 
+                        print "%s\t%s\t%s\t\t%s\t%s\t%s\t%s\t\t%s" % (row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7]) 
                     else:
-                        print "%s\t%s\t%s\t%s\t%s\t%s\t\t\t%s\t\t%s" % (row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7])        
+                        print "%s\t%s\t%s\t%s\t%s\t%s\t%s\t\t%s" % (row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7])        
 
 
     db.close()
